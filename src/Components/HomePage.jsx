2abf33bfdbe67657703cc7f67ea31020a9e58b42
Dhivyadharshinidd <80613292+Dhivyadharshinidd@users.noreply.github.com>
2021-09-28 16:23:35 +0530

    Create HomePage.jsx

---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Navigation from "./Navigation";
+import FirstParagraph from "./FirstParagraph";
+import styles from "./navigation.module.css";
+function HomePage() {
+    return (<>
+        <Navigation />
+        <br>
+        </br>
+
+        <div className="body">Numbers are important.  Whether costs, revenues, performance, targets – most people agree that numbers are important.  Interpretation of these numbers is key; the numbers can influence decisions related to performance, investments and effectiveness among other things. There appears to be a clear trend in Canada to 100% producer responsibility for the packaging they place onto the market, often cited as the best option for producers.  Comparing different approaches in different jurisdictions may not be an exact apples to apples comparison, but it does provide insight at a high level.
+        </div>
+        <br></br>
+        <div className="body">We use numbers in our day to day life. They are often called numerals. Without numbers, we cannot do counting of things, date, time, money, etc. Sometimes these numbers are used for measurement and sometimes they are used for labelling. The properties of numbers make them capable of performing arithmetic operations on them.  These numbers are expressed in numeric forms and also in words. For example, 2 is written as two in words, 25 is written as twenty-five in words, etc. Students can practice writing the numbers from 1 to 100 in words to learn more.
+
+            There are different types of numbers in Maths, which we learn. They are natural and whole numbers, odd and even numbers, rational and irrational numbers, etc. We will discuss all the types here in this article. Apart from these, the numbers are used in various applications such as forming number series, maths tables, etc.</div>
+        <br></br>
+        <div className="body">A number is an arithmetic value used for representing the quantity and used in making calculations. A written symbol like “3” which represents a number is known as numerals. A number system is a writing system for denoting numbers using digits or symbols in a logical manner. The numeral system:
+
+            Represents a useful set of numbers
+            Reflects the arithmetic and algebraic structure of a number
+            Provides standard representation</div>
+        <br></br>
+        <div className="body">The first known documented use of zero dates to AD 628, and appeared in the Brāhmasphuṭasiddhānta, the main work of the Indian mathematician Brahmagupta. He treated 0 as a number and discussed operations involving it, including division. By this time (the 7th century) the concept had clearly reached Cambodia as Khmer numerals, and documentation shows the idea later spreading to China and the Islamic world.
+
+
+            The number 605 in Khmer numerals, from an inscription from 683 AD. Early use of zero as a decimal figure.
+            Brahmagupta's Brāhmasphuṭasiddhānta is the first book that mentions zero as a number, hence Brahmagupta is usually considered the first to formulate the concept of zero. He gave rules of using zero with negative and positive numbers, such as "zero plus a positive number is a positive number, and a negative number plus zero is the negative number." The Brāhmasphuṭasiddhānta is the earliest known text to treat zero as a number in its own right, rather than as simply a placeholder digit in representing another number as was done by the Babylonians or as a symbol for a lack of quantity as was done by Ptolemy and the Romans.</div>
+    </>);
+}
+
+export default HomePage;
